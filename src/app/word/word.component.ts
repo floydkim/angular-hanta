c216628922ee2b19864694a6bd37dd6013036185
floydkim <r2d2em@empal.com>
2019-06-18 15:59:55 +0900

    data structure of wordsStorageService modified

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 
+import { Word } from '../types-definition';
+
+const INVADE_INTERVAL = 1000;
+const INVADE_HOP = 60;
 @Component({
   selector: 'app-word',
   templateUrl: './word.component.html',
@@ -8,7 +12,7 @@ import { interval, Subscription } from 'rxjs';
 })
 export class WordComponent implements OnInit, OnDestroy {
 
-  @Input() word: string;
+  @Input() word: Word;
 
   xpos: number;
   ypos: number;
@@ -27,20 +31,19 @@ export class WordComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.alive = false;
-    this.ypos = 350;
     this.invadeSubscrption.unsubscribe();
     console.log('WORD DESTROYED!!!', this.word, this.alive, this.ypos);
   }
 
   invade() {
-    const move = interval(1000);
+    const move = interval(INVADE_INTERVAL);
     this.invadeSubscrption = move.subscribe(i => {
       if (this.ypos >= 350) {
         // TODO: ypos말고 렌더된 엘리먼트의 바닥 좌표를 계산해서 기준삼아야함
         // 아니면 선 넘어가도 안보이게 잘 가리던지
         this.ngOnDestroy();
       }
-      this.ypos += 30;
+      this.ypos += INVADE_HOP;
     });
   }
 
--- src/app/words-storage.service.ts
+++ src/app/words-storage.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@angular/core';
 import { WordComponent } from './word/word.component';
 import { interval } from 'rxjs';
 
+import { Word } from './types-definition';
+
 
 // const dictionary = ['맥북', '한글', '윈도우', '후보', '앵귤러', '사과', '바나나', '수박', '커피', '아이스아메리카노', '자바칩프라푸치노', '리듬', '두뇌', '마술', '피아노', '기타', '베이스', '훈민정음', '이름'];
 const dictionary = ['단', '어'];
 
 const CREATE_RATE = 1000;
 
-class Word {
-  [word: string]: any;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class WordsStorageService {
-  words: string[];
+  words: Word[];
   removeTarget: string;
+  id: number;
 
   constructor() {
+    this.id = -1;
     this.words = [];
     const wordMaker = interval(CREATE_RATE);
     const subscribe = wordMaker.subscribe(val => {
@@ -34,15 +34,15 @@ export class WordsStorageService {
 
   deleteWord(value: string) {
     // console.log('(WSS) delete ', value, Object.keys(this.words).length);
-    const targetIndex = this.words.indexOf(value);
-    if (targetIndex > -1) {
-      this.words.splice(targetIndex, 1);
-    }
-    this.removeTarget = value;
+    // const targetIndex = this.words.indexOf(value);
+    // if (targetIndex > -1) {
+    //   this.words.splice(targetIndex, 1);
+    // }
+    // this.removeTarget = value;
   }
 
   updateWords() {
     const randomIndex = Math.trunc(dictionary.length * Math.random());
-    this.words.unshift(dictionary[randomIndex]);
+    this.words.push({ id: ++this.id, text: dictionary[randomIndex], alive: true });
   }
 }
