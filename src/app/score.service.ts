56bffac271f198860460dc602f86529dc6c9e089
floydkim <r2d2em@empal.com>
2019-06-19 05:02:15 +0900

    indicator balloon added

--- src/app/score.service.ts
+++ src/app/score.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { NgRedux } from '@angular-redux/store';
 import { ScoreActions } from './app.actions';
 import { IAppState } from '../store';
@@ -10,7 +10,8 @@ import { IAppState } from '../store';
 export class ScoreService {
 
   score: number;
-  subscription;
+  subscription: Subscription;
+  message: string;
 
   constructor(
     private ngRedux: NgRedux<IAppState>,
@@ -19,24 +20,29 @@ export class ScoreService {
     this.subscription = ngRedux
       .select<number>('score')
       .subscribe(newScore => (this.score = newScore));
+    this.message = '';
   }
 
   init() {
+    this.message = '';
     this.ngRedux.dispatch(this.actions.init());
     console.log('SCORE INITIALIZED!', this.ngRedux.getState().score, this.score);
   }
 
   increase() {
+    this.message = '득점';
     this.ngRedux.dispatch(this.actions.increase());
     console.log('GOT SCORE!', this.ngRedux.getState().score, this.score);
   }
 
   decrease() {
+    this.message = '감점';
     this.ngRedux.dispatch(this.actions.decrease());
     console.log('LOST SCORE!', this.ngRedux.getState().score, this.score);
   }
 
   zero() {
+    this.message = '';
     this.ngRedux.dispatch(this.actions.zero());
   }
 }
